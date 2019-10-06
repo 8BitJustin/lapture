e732f71d83795972267491e872e7f085ea4b87ac
Justin Olson <j.olson.digital@gmail.com>
2019-10-05 17:22:25 -0700

    Removed helloworld file. Added datetime and implemented timestamp text to file name of saved image.

--- lapture.py
+++ lapture.py
@@ -1,11 +1,18 @@
 import cv2, time
+from datetime import datetime
+
+timestamp = datetime.now()
+timestamp = timestamp.strftime("%Y-%m-%d-%H-%M-%S")
 
 cam = cv2.VideoCapture(0)
 
 check, frame = cam.read()
 
-time.sleep(5)
-cv2.imwrite("image.jpg", frame)
+print(check)
+print(frame)
+
+time.sleep(3)
+cv2.imwrite(timestamp + ".jpg", frame)
 
 cam.release()
 
