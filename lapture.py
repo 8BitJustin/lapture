ef55e18b04ffddc511853007024cd73e1283b985
Justin Olson <j.olson.digital@gmail.com>
2019-10-05 21:04:19 -0700

    Last save of evening commit.

--- lapture.py
+++ lapture.py
@@ -1,6 +1,7 @@
 import cv2, time
 from datetime import datetime
 
+# timestamp for file name as time image saved
 timestamp = datetime.now()
 timestamp = timestamp.strftime("%Y-%m-%d-%H-%M-%S")
 
@@ -9,10 +10,9 @@ cam = cv2.VideoCapture(0)
 check, frame = cam.read()
 
 print(check)
-print(frame)
 
 time.sleep(3)
+
 cv2.imwrite(timestamp + ".jpg", frame)
 
 cam.release()
-
